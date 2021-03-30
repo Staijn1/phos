24d47d6e5d20df3af48d3487f14ae7b0f1f085a6
Staijn1 <stein@jnkr.eu>
2021-03-30 20:31:53 +0200

    Install with yarn and changed rebuilding

---
+++ Front-End/rebuild.js
@@ -0,0 +1,23 @@
+const exec = require('child_process').exec;
+const os = require('os');
+
+const electronVersion = '11.0.0';
+
+function callback(error, stdout, stderr) {
+    if (error) {
+        console.error(error);
+    }
+    console.log('stdout', stdout);
+    console.log('stderr', stderr);
+}
+
+// Run command depending on the OS
+if (os.type() === 'Linux') {
+    exec(`./node_modules/.bin/electron-rebuild --target=${electronVersion}`, callback);
+} else if (os.type() === 'Darwin') {
+    exec(`./node_modules/.bin/electron-rebuild --target=${electronVersion}`, callback);
+} else if (os.type() === 'Windows_NT') {
+    exec(`node_modules\\.bin\\electron-rebuild --target=${electronVersion}`, callback);
+} else {
+    throw new Error('Unsupported OS found: ' + os.type());
+}
