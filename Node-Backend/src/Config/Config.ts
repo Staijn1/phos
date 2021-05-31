2bb38b883f5db7ff43b3d21a1d37b5c9c260eb31
Staijn1 <stein@jnkr.eu>
2021-05-31 23:09:42 +0200

    Update keys

--- Node-Backend/src/Config/Config.ts
+++ Node-Backend/src/Config/Config.ts
@@ -5,6 +5,6 @@ export const API_PORT = 5000
 export const WEBSOCKET_PORT = 5001
 export const ledstripAdresses = ['ws://bed.local:81', 'ws://bureau.local:81']
 export const SSLOptions = {
-  key: fs.readFileSync(path.join(__dirname, '..', 'Certs', 'raspberry.key')),
-  cert: fs.readFileSync(path.join(__dirname, '..', 'Certs', 'raspberry.crt')),
+  key: fs.readFileSync(path.join(__dirname, '..', 'Certs', 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, '..', 'Certs', 'cert.pem')),
 }
