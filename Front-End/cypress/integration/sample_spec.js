5fb6c46e17edb6eb76d9eb25b685f7700ce8fab9
Staijn1 <stein@jnkr.eu>
2021-06-09 20:21:46 +0200

    Update sample_spec.js

--- Front-End/cypress/integration/sample_spec.js
+++ Front-End/cypress/integration/sample_spec.js
@@ -2,7 +2,10 @@ describe('My First Test', () => {
   beforeEach(() => {
     cy.visit('http://localhost:4200/')
   })
-  it('Does not do much!', () => {
-    expect(cy.url()).equal('http://localhost:4200/home')
+  it('We should land on the home page', () => {
+    cy.wait(3000);
+    cy.location(loc => {
+      console.log(loc)
+    })
   })
 })
