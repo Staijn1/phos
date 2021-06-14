416c635ced70c5235789c255536f5b52959ae4c1
Staijn1 <stein@jnkr.eu>
2021-06-14 20:37:42 +0200

    Turned into PWA

--- Front-End/src/app/app.component.ts
+++ Front-End/src/app/app.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core'
+import { SwUpdate } from '@angular/service-worker'
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
+  selector: 'app-root',
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
+  updateAvailable = false
+  constructor(readonly updates: SwUpdate) {
+    updates.available.subscribe(event => {
+      this.updateAvailable = true
+    })
+
+    // updates.activateUpdate().then(() => document.location.reload())
+  }
 }
