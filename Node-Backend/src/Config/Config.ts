0df2df70a2320fc226ad375ec5182ced347ee093
Staijn1 <stein@jnkr.eu>
2021-10-20 16:40:35 +0200

    Update Config.ts

--- Node-Backend/src/Config/Config.ts
+++ Node-Backend/src/Config/Config.ts
@@ -3,7 +3,10 @@ import path from 'path'
 
 export const API_PORT = 5000
 export const WEBSOCKET_PORT = 5001
-export const ledstripAdresses = ['ws://192.168.178.3:81']
+export const ledstripAdresses = [
+  'ws://192.168.178.3:81',
+  'ws://192.168.178.4:81',
+]
 export const SSLOptions = {
   key: fs.readFileSync(path.join(__dirname, '..', 'Certs', 'key.pem')),
   cert: fs.readFileSync(path.join(__dirname, '..', 'Certs', 'cert.pem')),
