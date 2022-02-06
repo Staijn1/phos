caa4fbb933c6b66b8e321f3999fdd38aab6c231d
Staijn1 <stein@jnkr.eu>
2022-02-06 21:16:38 +0100

    IP changes

--- Node-Backend/src/Config/Config.ts
+++ Node-Backend/src/Config/Config.ts
@@ -4,8 +4,8 @@ import path from 'path'
 export const API_PORT = 5000
 export const WEBSOCKET_PORT = 5001
 export const ledstripAdresses = [
-  'ws://192.168.2.2:81',
-  // 'ws://192.168.178.4:81',
+  'ws://192.168.2.249:81',
+  'ws://192.168.2.248:81',
 ]
 export const SSLOptions = {
   key: fs.readFileSync(path.join(__dirname, '..', 'Certs', 'key.pem')),
