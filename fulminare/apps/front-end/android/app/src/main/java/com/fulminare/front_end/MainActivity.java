8a805d553bce1c010431d9eb2300990215b99113
Staijn1 <stein@jnkr.eu>
2022-02-19 17:55:23 +0100

    replaced react-native with flutter and put a template express api in the nx monorepo

---
+++ fulminare/apps/front-end/android/app/src/main/java/com/fulminare/front_end/MainActivity.java
@@ -0,0 +1,6 @@
+package com.fulminare.front_end;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- fulminare/apps/mobile/readme.md
+++
@@ -1,3 +0,0 @@
-nx run mobile
-
-nx run-android mobile
