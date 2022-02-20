bcb278885eea0fd9e585ec443d69dffa3197464e
Staijn1 <stein@jnkr.eu>
2022-02-20 13:06:24 +0100

    Removed one unnecessary example

--- fulminare/apps/front-end/lib/main.dart
+++ fulminare/apps/front-end/lib/main.dart
@@ -1,115 +1,186 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
 
 void main() {
-  runApp(const MyApp());
+  /// Comment or uncomment to run both examples
+  runApp(WithBuilder());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+///Class to hold data for itembuilder in Withbuilder app.
+class ItemData {
+  final Color color;
+  final String image;
+  final String text1;
+  final String text2;
+  final String text3;
 
-  // This widget is the root of your application.
+  ItemData(this.color, this.image, this.text1, this.text2, this.text3);
+}
+
+/// Example of LiquidSwipe with itemBuilder
+class WithBuilder extends StatefulWidget {
+  static final style = TextStyle(
+    fontSize: 30,
+    fontFamily: "Billy",
+    fontWeight: FontWeight.w600,
+  );
+
+  @override
+  _WithBuilder createState() => _WithBuilder();
+}
+
+class _WithBuilder extends State<WithBuilder> {
+  int page = 0;
+  late LiquidController liquidController;
+  late UpdateType updateType;
+
+  List<ItemData> data = [
+    ItemData(Colors.blue, "assets/1.png", "Hi", "It's Me", "Sahdeep"),
+    ItemData(Colors.deepPurpleAccent, "assets/1.png", "Take a", "Look At",
+        "Liquid Swipe"),
+    ItemData(Colors.green, "assets/1.png", "Liked?", "Fork!", "Give Star!"),
+    ItemData(Colors.yellow, "assets/1.png", "Can be", "Used for",
+        "Onboarding design"),
+    ItemData(Colors.red, "assets/1.png", "Do", "try it", "Thank you"),
+  ];
+
+  @override
+  void initState() {
+    liquidController = LiquidController();
+    super.initState();
+  }
+
+  Widget _buildDot(int index) {
+    double selectedness = Curves.easeOut.transform(
+      max(
+        0.0,
+        1.0 - 0.5,
+      ),
+    );
+    double zoom = 1.0 + (2.0 - 1.0) * selectedness;
+    return new Container(
+      width: 25.0,
+      child: new Center(
+        child: new Material(
+          color: Colors.white,
+          type: MaterialType.circle,
+          child: new Container(
+            width: 8.0 * zoom,
+            height: 8.0 * zoom,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      home: Scaffold(
+        body: Stack(
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            LiquidSwipe.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  width: double.infinity,
+                  color: data[index].color,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Image.asset(
+                        data[index].image,
+                        height: 400,
+                        fit: BoxFit.contain,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(20.0),
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            data[index].text1,
+                            style: WithBuilder.style,
+                          ),
+                          Text(
+                            data[index].text2,
+                            style: WithBuilder.style,
+                          ),
+                          Text(
+                            data[index].text3,
+                            style: WithBuilder.style,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+              positionSlideIcon: 0.8,
+              slideIconWidget: Icon(Icons.arrow_back_ios),
+              onPageChangeCallback: pageChangeCallback,
+              waveType: WaveType.liquidReveal,
+              liquidController: liquidController,
+              fullTransitionValue: 880,
+              enableSideReveal: true,
+              enableLoop: true,
+              ignoreUserGestureWhileAnimating: true,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: <Widget>[
+                  Expanded(child: SizedBox()),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List<Widget>.generate(data.length, _buildDot),
+                  ),
+                ],
+              ),
             ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: FlatButton(
+                  onPressed: () {
+                    liquidController.animateToPage(
+                        page: data.length - 1, duration: 700);
+                  },
+                  child: Text("Skip to End"),
+                  color: Colors.white.withOpacity(0.01),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: FlatButton(
+                  onPressed: () {
+                    liquidController.jumpToPage(
+                        page: liquidController.currentPage + 1 > data.length - 1
+                            ? 0
+                            : liquidController.currentPage + 1);
+                  },
+                  child: Text("Next"),
+                  color: Colors.white.withOpacity(0.01),
+                ),
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  pageChangeCallback(int lpage) {
+    setState(() {
+      page = lpage;
+    });
   }
 }
