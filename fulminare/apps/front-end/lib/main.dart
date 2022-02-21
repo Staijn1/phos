e469bd598551f7db61228e921ba5e78b445da856
Staijn1 <stein@jnkr.eu>
2022-02-22 00:11:35 +0100

    Created light/dark mode switch and ditched liquid swipe

--- fulminare/apps/front-end/lib/main.dart
+++ fulminare/apps/front-end/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:Fulminare/src/pages/home.dart';
 import 'package:Fulminare/src/widgets/liquid-pages.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hsvcolor_picker/flutter_hsvcolor_picker.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
 
 void main() => runApp(const Fulminare());
@@ -9,22 +11,55 @@ class Fulminare extends StatefulWidget {
 
   @override
   _FulminareState createState() => _FulminareState();
+
+  static _FulminareState? of(BuildContext context) =>
+      context.findAncestorStateOfType<_FulminareState>();
 }
 
 class _FulminareState extends State<Fulminare> {
+  int _selectedIndex = 0;
+  ThemeMode _themeMode = ThemeMode.system;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          body: LiquidSwipe(
-            pages: liquidPages,
-            fullTransitionValue: 1300,
-            enableLoop: true,
-            ignoreUserGestureWhileAnimating: true,
-            slideIconWidget: const Icon(Icons.arrow_back_ios),
-            enableSideReveal: false,
-            waveType: WaveType.liquidReveal,
-          )),
-    );
+        theme: ThemeData(),
+        darkTheme: ThemeData.dark(), // standard dark theme
+        themeMode: _themeMode,
+        home: Scaffold(
+          body: HomePage(),
+          bottomNavigationBar: BottomNavigationBar(
+            backgroundColor: Colors.black,
+            type: BottomNavigationBarType.shifting,
+            selectedFontSize: 20,
+            selectedIconTheme: const IconThemeData(color: Colors.amber),
+            selectedItemColor: Colors.amber,
+            selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.camera),
+                label: 'Camera',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.camera),
+                label: 'Second',
+              ),
+            ],
+          ),
+        ));
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void changeTheme(ThemeMode themeMode) {
+    setState(() {
+      _themeMode = themeMode;
+    });
   }
 }
--- fulminare/apps/front-end/lib/src/pages/home.dart
+++ fulminare/apps/front-end/lib/src/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:Fulminare/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hsvcolor_picker/flutter_hsvcolor_picker.dart';
 
@@ -17,15 +18,31 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: Colors.redAccent,
       body: SafeArea(
           child: Padding(
-        padding: EdgeInsets.all(30),
-        child: Column(children: [
-          const Text("Homepage", style: headingStyle),
-          Scrollbar(
-            child: ColorPicker(
-              onChanged: (Color value) {},
-            ),
-          )
-        ]),
+        padding: const EdgeInsets.all(30),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Choose your theme:',
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  /// //////////////////////////////////////////////////////
+                  /// Change theme & rebuild to show it using these buttons
+                  ElevatedButton(
+                      onPressed: () => Fulminare.of(context)?.changeTheme(ThemeMode.light),
+                      child: Text('Light')),
+                  ElevatedButton(
+                      onPressed: () => Fulminare.of(context)?.changeTheme(ThemeMode.dark),
+                      child: Text('Dark')),
+                  /// //////////////////////////////////////////////////////
+                ],
+              ),
+            ],
+          ),
+        ),
       )),
     );
   }
