f209ba02374f78372f240dac881cb2f2a5bfb491
Staijn1 <stein@jnkr.eu>
2022-02-21 23:14:51 +0100

    Main class is now stateful

--- fulminare/apps/front-end/lib/main.dart
+++ fulminare/apps/front-end/lib/main.dart
@@ -2,22 +2,29 @@ import 'package:Fulminare/src/widgets/liquid-pages.dart';
 import 'package:flutter/material.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
 
-void main() => runApp(Fulminare());
+void main() => runApp(const Fulminare());
 
-class Fulminare extends StatelessWidget {
+class Fulminare extends StatefulWidget {
+  const Fulminare({Key? key}) : super(key: key);
+
+  @override
+  _FulminareState createState() => _FulminareState();
+}
+
+class _FulminareState extends State<Fulminare> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Builder(
           builder: (context) => LiquidSwipe(
-                pages: liquidPages,
-                fullTransitionValue: 1100,
-                enableLoop: true,
-                ignoreUserGestureWhileAnimating: true,
-                slideIconWidget: const Icon(Icons.arrow_back_ios),
-                enableSideReveal: true,
-                waveType: WaveType.liquidReveal,
-              )),
+            pages: liquidPages,
+            fullTransitionValue: 1100,
+            enableLoop: true,
+            ignoreUserGestureWhileAnimating: true,
+            slideIconWidget: const Icon(Icons.arrow_back_ios),
+            enableSideReveal: true,
+            waveType: WaveType.liquidReveal,
+          )),
     );
   }
 }
