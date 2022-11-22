b3b6c637b18fe69759b5c47d1556d5129d5d0781
Staijn1 <stein@jnkr.eu>
2022-11-22 21:47:44 +0100

    Update environment.prod.ts

--- apps/website/src/environments/environment.prod.ts
+++ apps/website/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   saveSettings: true,
   url: 'http://192.168.2.249',
-  websockUrl: 'ws://192.168.2.249:81'
+  websockUrl: 'https://api.angulon.steinjonker.nl'
 }
