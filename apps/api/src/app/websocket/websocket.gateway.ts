e9e7854a3c528a1dba05b932983869b79b979f74
Staijn1 <stein@jnkr.eu>
2022-11-23 14:07:17 +0100

    Comments

--- apps/api/src/app/websocket/websocket.gateway.ts
+++ apps/api/src/app/websocket/websocket.gateway.ts
@@ -1,18 +1,18 @@
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
-  OnGatewayInit,
   SubscribeMessage,
-  WebSocketGateway, WebSocketServer
+  WebSocketGateway,
+  WebSocketServer
 } from '@nestjs/websockets';
 import {Logger} from '@nestjs/common';
-import {Socket, Server} from 'socket.io';
+import {Server, Socket} from 'socket.io';
 import {WebsocketClientsManagerService} from './websocket-clients-manager.service';
 import {ConfigurationService} from '../configuration/configuration.service';
 import {GradientInformation, ModeInformation} from '@angulon/interfaces';
 
 @WebSocketGateway(undefined, {cors: true})
-export class WebsocketGateway implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit {
+export class WebsocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   private server: Server;
   private logger: Logger = new Logger('WebsocketGateway');
@@ -60,17 +60,26 @@ export class WebsocketGateway implements OnGatewayConnection, OnGatewayDisconnec
     return this.configurationService.getGradients();
   }
 
+  /**
+   * When a client connects, log its IP address.
+   * Also set the server instance in the websocketClientsManagerService, so we make sure it is always up-to-date with the current server instance.
+   * @param {Socket} client
+   * @param args
+   * @returns {any}
+   */
   handleConnection(client: Socket, ...args: any[]): any {
     this.logger.log(`Client connected: ${client.id}. IP: ${client.conn.remoteAddress}`);
     this.websocketClientsManagerService.setServer(this.server);
   }
 
+  /**
+   * When a client disconnects, log its IP address and that it has disconnected.
+   * Just like the handleConnection method, we set the server instance again on the websocketClientsManagerService.
+   * @param {Socket} client
+   * @returns {any}
+   */
   handleDisconnect(client: Socket): any {
     this.logger.log(`Client disconnected: ${client.id}. IP: ${client.conn.remoteAddress}`);
     this.websocketClientsManagerService.setServer(this.server);
-  }
-
-  afterInit(server: any): any {
-    this.websocketClientsManagerService.setServer(this.server)
   }
 }
