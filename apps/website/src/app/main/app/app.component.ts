65cf48d770a5563b8271434336a250e4bf95349d
Staijn1 <stein@jnkr.eu>
2022-11-23 15:40:35 +0100

    Revamp of alerts

--- apps/website/src/app/main/app/app.component.ts
+++ apps/website/src/app/main/app/app.component.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core'
 import {SwUpdate} from '@angular/service-worker'
 import {ErrorService} from '../../services/error/error.service'
-import {ThemeService} from "../../services/theme/theme.service";
-import {environment} from "../../../environments/environment";
+import {ThemeService} from '../../services/theme/theme.service';
+import {environment} from '../../../environments/environment';
+import {Message} from '../../messages/Message';
 
 @Component({
   selector: 'app-root',
@@ -10,20 +11,24 @@ import {environment} from "../../../environments/environment";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  updateAvailable = false
-
   constructor(readonly updates: SwUpdate, public errorService: ErrorService, private theme: ThemeService) {
     this.theme.loadTheme();
+
     // Service worker update, but only in production. During development, the service worker is disabled which results in an error.
     // Enabling the service worker would result in a lot of caching, which is not desired during development because it would be hard to test changes.
     if (environment.production) {
-      updates.checkForUpdate().then((hasNewUpdate) => {
-        this.updateAvailable = hasNewUpdate
+      updates.checkForUpdate().then(() => {
+        this.errorService.setError(new Message('info', 'New update available! Click here to update.', () => this.update()))
       })
     }
   }
 
   update() {
     this.updates.activateUpdate().then(() => document.location.reload())
+  }
+
+  onAlertClick(error: Message) {
+    if (!error.action) return;
+    error.action();
   }
 }
--- apps/website/src/app/services/error/error.service.ts
+++ apps/website/src/app/services/error/error.service.ts
@@ -1,19 +1,34 @@
 import {Injectable} from '@angular/core'
+import {Message} from '../../messages/Message';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorService {
-  private _errors: Error[] = [];
+  private _errors: Message[] = [];
 
   public setError(error: Error) {
-    this._errors.push(error)
+    const mappedError = this.mapError(error)
+    this._errors.push(mappedError)
     setTimeout(() => {
       this._errors.shift()
     }, 5000)
   }
 
-  public getErrors(): Error[] {
+  public getErrors(): Message[] {
     return this._errors
+  }
+
+  /**
+   * Map an incoming error to a message object. If the incoming error is already a message, it will be returned as is.
+   * @param {Error | Message} message
+   * @returns {Message}
+   * @private
+   */
+  private mapError(message: Error | Message): Message {
+    if (message instanceof Message) {
+      return message
+    }
+    return new Message('error', message.message)
   }
 }
