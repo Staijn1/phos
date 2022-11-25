4d0260cf4697731cc908495e19607de3e64ba7af
Staijn1 <stein@jnkr.eu>
2022-11-25 22:42:44 +0100

    Refactoring of the colorpicker to make it more re-usable (for the gradient-editor)

--- apps/website/src/app/main/colorpicker/colorpicker.component.ts
+++ apps/website/src/app/main/colorpicker/colorpicker.component.ts
@@ -1,55 +1,63 @@
 import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output} from '@angular/core'
 import iro from '@jaames/iro'
 import {IroColorPicker} from '@jaames/iro/dist/ColorPicker'
-import {SettingsService} from "../../services/settings/settings.service";
-import Color = iro.Color;
-import {ChromaEffectService} from "../../services/chromaEffect/chroma-effect.service";
-import {LedstripCommandService} from "../../services/ledstrip-command/ledstrip-command.service";
+
+export type ColorpickerEvent = {
+  color: iro.Color
+  colorpicker: iro.ColorPicker
+}
 
 @Component({
   selector: 'app-colorpicker',
   templateUrl: './colorpicker.component.html',
   styleUrls: ['./colorpicker.component.scss'],
 })
-export class ColorpickerComponent implements OnInit {
-  @Input() color!: string[]
-  @Output() colorChange = new EventEmitter<Color>()
+export class ColorpickerComponent implements OnInit, AfterViewInit {
+  @Input() initialColor!: string[]
+  @Output() colorInit = new EventEmitter<ColorpickerEvent>()
+  @Output() colorChange = new EventEmitter<ColorpickerEvent>()
+  @Output() inputEnd = new EventEmitter<ColorpickerEvent>();
   private picker!: IroColorPicker
-
-  constructor(private readonly settingsService: SettingsService, private chromaEffect: ChromaEffectService, private connection: LedstripCommandService,) {
-  }
+  id!: string;
 
   ngOnInit(): void {
-    const currentSettings = this.settingsService.readGeneralSettings()
+    this.generateElementId();
+  }
+
+  ngAfterViewInit(): void {
     try {
-      this.picker = iro.ColorPicker('#colorpicker', {
+      this.picker = iro.ColorPicker(`#${this.id}`, {
         width: 150,
         layoutDirection: 'horizontal',
         handleRadius: 8,
         borderWidth: 2,
         borderColor: '#fff',
         wheelAngle: 90,
-        color: currentSettings.colors[0],
+        colors: this.initialColor
       })
       this.picker.on('color:init', (iroColor: iro.Color) => {
-        if (this.settingsService.readGeneralSettings().initialColor) {
-          this.connection.setColor(this.picker.colors)
-        }
-        this.chromaEffect.setColors = this.picker.colors
+        this.colorInit.emit({color: iroColor, colorpicker: this.picker})
       })
       this.picker.on('color:change', (iroColor: iro.Color) => {
-        this.connection.setColor(this.picker.colors)
-        this.chromaEffect.setColors = this.picker.colors
+        this.colorChange.emit({color: iroColor, colorpicker: this.picker})
       })
       this.picker.on('input:end', (iroColor: iro.Color) => {
-        const settings = this.settingsService.readGeneralSettings()
-        settings.colors = this.settingsService.convertColors(this.picker.colors)
-        this.settingsService.saveGeneralSettings(settings)
+        this.inputEnd.emit({color: iroColor, colorpicker: this.picker})
       })
-
     } catch (e) {
       console.error(`Colorpicker creation failed for #colorpicker. Reason: `, e)
     }
+  }
+
+  /**
+   * To prevent multiple colorpickers on the same page ending up with the same id, we generate a random id.
+   * It therefore should be a valid html ID
+   * @private
+   */
+  private generateElementId() {
+    const array = new Uint32Array(5);
+    self.crypto.getRandomValues(array);
+    this.id = 'colorpicker-' + array.join('-');
   }
 
 }
