cd72f48ae2c4b001bf13e4ff71890870160ff933
Staijn1 <stein@jnkr.eu>
2022-11-27 14:50:09 +0100

    Better consistency in folder names for pages

--- apps/website/src/app/app-routing/app-routing.module.ts
+++ apps/website/src/app/app-routing/app-routing.module.ts
@@ -1,19 +1,19 @@
 import {NgModule} from '@angular/core'
 import {RouterModule, Routes} from '@angular/router'
-import {SettingsPageComponent} from '../pages/settingsPage/settings-page.component'
-import {ModePageComponent} from '../pages/modePage/mode-page.component'
-import {VisualizerPageComponent} from '../pages/visualizerPage/visualizer-page.component'
+import {SettingsPageComponent} from '../pages/settings-page/settings-page.component'
+import {ModePageComponent} from '../pages/mode-page/mode-page.component'
+import {VisualizerPageComponent} from '../pages/visualizer-page/visualizer-page.component'
 import {VisualizerPage3DComponent} from '../pages/visualizer-page3-d/visualizer-page3-d.component'
 
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
-  {path: 'home', loadChildren: () => import('../pages/homePage/home-page.module').then(m => m.HomePageModule)},
+  {path: 'home', loadChildren: () => import('../pages/home-page/home-page.module').then(m => m.HomePageModule)},
   {path: 'visualizer', component: VisualizerPageComponent},
   {path: 'mode', component: ModePageComponent},
   {
     path: 'gradients-editor',
-    loadChildren: () => import('../pages/gradientEditorPage/gradient-editor.module').then(m => m.GradientEditorModule)
+    loadChildren: () => import('../pages/gradient-editor-page/gradient-editor.module').then(m => m.GradientEditorModule)
   },
   {path: 'settings', component: SettingsPageComponent},
   {path: 'visualizer3d', component: VisualizerPage3DComponent}
