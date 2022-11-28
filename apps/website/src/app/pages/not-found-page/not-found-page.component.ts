e4e149689cb3e382a7d438f2e09a208e2bd219da
Staijn1 <stein@jnkr.eu>
2022-11-28 17:23:28 +0100

    Update not-found-page.component.ts

--- apps/website/src/app/pages/not-found-page/not-found-page.component.ts
+++ apps/website/src/app/pages/not-found-page/not-found-page.component.ts
@@ -1,19 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {ThemeService} from "../../services/theme/theme.service";
-import {gsap} from 'gsap'
 
 @Component({
   selector: 'app-not-found-page',
   templateUrl: './not-found-page.component.html',
   styleUrls: ['./not-found-page.component.scss'],
 })
-export class NotFoundPageComponent implements OnInit {
+export class NotFoundPageComponent {
   theme!: string;
 
   constructor(private themeService: ThemeService) {
     this.theme = this.themeService.theme;
-  }
-
-  ngOnInit(): void {
   }
 }
