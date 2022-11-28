528043dbb3dcef57fae17a1a8259cde69d155d20
Staijn1 <stein@jnkr.eu>
2022-11-28 17:19:37 +0100

    Animated 404 screen

--- apps/website/src/app/pages/not-found-page/not-found-page.component.ts
+++ apps/website/src/app/pages/not-found-page/not-found-page.component.ts
@@ -1,15 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ThemeService} from "../../services/theme/theme.service";
+import {gsap} from 'gsap'
 
 @Component({
   selector: 'app-not-found-page',
   templateUrl: './not-found-page.component.html',
   styleUrls: ['./not-found-page.component.scss'],
 })
-export class NotFoundPageComponent {
+export class NotFoundPageComponent implements OnInit {
   theme!: string;
 
   constructor(private themeService: ThemeService) {
     this.theme = this.themeService.theme;
+  }
+
+  ngOnInit(): void {
   }
 }
