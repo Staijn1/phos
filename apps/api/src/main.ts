79ce600ea77c102e9988bb4fa7ff82dbda3f3fd3
Stein Jonker <stein@jnkr.eu>
2023-01-10 17:23:18 +0000

    Delete import from pg

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,7 +7,6 @@ import {Logger} from '@nestjs/common';
 import {NestFactory} from '@nestjs/core';
 
 import {AppModule} from './app/app.module';
-import { Client } from "pg";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
