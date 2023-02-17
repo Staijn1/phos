ebab32c2fe9d9f6d8c13cc351fd8a9589cfdec69
Staijn1 <stein@jnkr.eu>
2023-02-17 16:20:08 +0100

    Fixed URL in local development

--- apps/website/src/environments/environment.ts
+++ apps/website/src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   saveSettings: false,
-  url: 'https://api.angulon.steinjonker.nl'
+  url: 'http://localhost:3333'
 }
 
 export const AppConfig = {
