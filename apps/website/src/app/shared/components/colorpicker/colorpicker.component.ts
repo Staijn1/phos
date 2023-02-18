fe3ebc70ae17d293ad69e771b42e404bcdb8afea
Staijn1 <stein@jnkr.eu>
2023-02-18 16:49:14 +0100

    Fixed an issue where active color was updated when setting color, and also added bars for the other colors instead of the primary

--- apps/website/src/app/shared/components/colorpicker/colorpicker.component.ts
+++ apps/website/src/app/shared/components/colorpicker/colorpicker.component.ts
@@ -23,6 +23,7 @@ export class ColorpickerComponent implements OnInit, AfterViewInit {
   id!: string;
   private picker!: IroColorPicker
   private skipColorChangeEmit = false;
+  private activeColorIndex = 0;
 
   constructor(private store: Store<{ colorpicker: ColorpickerState }>) {
   }
@@ -40,15 +41,47 @@ export class ColorpickerComponent implements OnInit, AfterViewInit {
         borderWidth: 2,
         borderColor: '#fff',
         wheelAngle: 90,
+        layout: [
+          {
+            component: iro.ui.Wheel,
+          },
+          {
+            component: iro.ui.Slider,
+            options: {
+              sliderType: 'value',
+              activeIndex: 0,
+            }
+          },
+          {
+            component: iro.ui.Slider,
+            options: {
+              sliderType: 'value',
+              activeIndex: 1,
+            }
+          },
+          {
+            component: iro.ui.Slider,
+            options: {
+              sliderType: 'value',
+              activeIndex: 2,
+            }
+          },
+        ]
       });
+
       this.store.select('colorpicker').subscribe((state) => {
         this.skipColorChangeEmit = true;
         this.picker.setColors(state.colors);
+
+        // When setting the colors, the active color is reset to the first color.
+        // We need to set it back to the index of the color that was active before, otherwise it jumps to the first color and drags that instead
+        this.picker.setActiveColor(this.activeColorIndex)
         this.skipColorChangeEmit = false;
       });
 
       this.picker.on('color:change', (color: iro.Color) => {
         if (!this.skipColorChangeEmit) {
+          this.activeColorIndex = color.index
           const colors = this.picker.colors.map(c => c.hexString);
           this.store.dispatch(colorChange(colors));
         }
