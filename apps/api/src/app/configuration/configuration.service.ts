96c8e77a908e41149e31d44f55757449031b46e3
Staijn1 <stein@jnkr.eu>
2023-05-06 17:12:02 +0200

    Restructuring of assets folder to allow writing from defaults file and mapping volumes

--- apps/api/src/app/configuration/configuration.service.ts
+++ apps/api/src/app/configuration/configuration.service.ts
@@ -1,14 +1,19 @@
-import {Injectable} from '@nestjs/common';
+import {Injectable, Logger} from '@nestjs/common';
 import {GradientInformation, LedstripPreset, ModeInformation} from '@angulon/interfaces';
 import * as fs from 'fs';
 import path = require('path');
 
 @Injectable()
 export class ConfigurationService {
-  private readonly assetPath: string;
+  private readonly rootAssetsPath: string;
+  private reaodnly
+  configPath: string;
+  private readonly logger: Logger = new Logger('ConfigurationService');
 
   constructor() {
-    this.assetPath = path.join(__filename, '../assets');
+    this.rootAssetsPath = path.join(__filename, '../assets');
+    this.configPath = path.join(this.rootAssetsPath, 'config');
+    this.writeDefaults().then();
   }
 
   /**
@@ -16,7 +21,7 @@ export class ConfigurationService {
    * @returns {Promise<ModeInformation[]>}
    */
   async getModes(): Promise<ModeInformation[]> {
-    const contents = await fs.promises.readFile(path.join(this.assetPath, 'modes.json'));
+    const contents = await fs.promises.readFile(path.join(this.rootAssetsPath, 'modes.json'));
     return JSON.parse(contents.toString());
   }
 
@@ -25,7 +30,7 @@ export class ConfigurationService {
    * @returns {Promise<GradientInformation[]>}
    */
   async getGradients(): Promise<GradientInformation[]> {
-    const contents = await fs.promises.readFile(path.join(this.assetPath, 'gradients.json'));
+    const contents = await fs.promises.readFile(path.join(this.rootAssetsPath, 'gradients.json'));
     return JSON.parse(contents.toString());
   }
 
@@ -35,21 +40,56 @@ export class ConfigurationService {
    * @returns {Promise<void>}
    */
   async writeGradients(gradients: GradientInformation[]): Promise<void> {
-    await fs.promises.writeFile(path.join(this.assetPath, 'gradients.json'), JSON.stringify(gradients, null, 2));
+    await fs.promises.writeFile(path.join(this.rootAssetsPath, 'gradients.json'), JSON.stringify(gradients, null, 2));
   }
 
   /**
    * Write a new array of presets to the presets.json file in the assets folder
    */
   async writePresets(presets: LedstripPreset[]): Promise<void> {
-    await fs.promises.writeFile(path.join(this.assetPath, 'presets.json'), JSON.stringify(presets, null, 2));
+    await fs.promises.writeFile(path.join(this.rootAssetsPath, 'presets.json'), JSON.stringify(presets, null, 2));
   }
 
   /**
    * Read the presets.json file in the assets folder
    */
   async getPresets(): Promise<LedstripPreset[]> {
-    const contents = await fs.promises.readFile(path.join(this.assetPath, 'presets.json'));
+    const contents = await fs.promises.readFile(path.join(this.rootAssetsPath, 'presets.json'));
     return JSON.parse(contents.toString());
   }
+
+  /**
+   * For each file in assetpath/defaults, check if the file exists in assetpath
+   * If not, write that file to the assetpath
+   * Also check if the config folder exists, if not, create it
+   * @private
+   */
+  private async writeDefaults(): Promise<void> {
+    const defaultsPath = path.join(this.rootAssetsPath, 'defaults');
+    const defaultFiles = await fs.promises.readdir(defaultsPath);
+
+    // Check if config folder exists
+    try {
+      await fs.promises.access(this.configPath, fs.constants.F_OK);
+    } catch (err) {
+      // If not, create it
+      await fs.promises.mkdir(this.configPath);
+    }
+
+    for (const file of defaultFiles) {
+      const defaultFilePath = path.join(defaultsPath, file);
+      const assetFilePath = path.join(this.configPath, file);
+
+      try {
+        // Check if the file exists in the asset path
+        await fs.promises.access(assetFilePath, fs.constants.F_OK);
+      } catch (err) {
+        // If the file does not exist, write it to the asset path
+        this.logger.log(`Writing defaults for file ${file}`)
+        const contents = await fs.promises.readFile(defaultFilePath);
+        await fs.promises.writeFile(assetFilePath, contents);
+      }
+    }
+  }
 }
+
