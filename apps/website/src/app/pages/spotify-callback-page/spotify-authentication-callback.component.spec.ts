4aedc5079b4ba5ee65dca2196d1947595fd5ff27
Staijn1 <stein@jnkr.eu>
2023-05-18 19:07:51 +0200

    Create spotify-callback page

---
+++ apps/website/src/app/pages/spotify-callback-page/spotify-authentication-callback.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SpotifyAuthenticationCallbackComponent } from './spotify-authentication-callback.component';
+
+describe('SpotifyAuthenticationCallbackComponent', () => {
+  let component: SpotifyAuthenticationCallbackComponent;
+  let fixture: ComponentFixture<SpotifyAuthenticationCallbackComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SpotifyAuthenticationCallbackComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SpotifyAuthenticationCallbackComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ apps/website/src/app/pages/spotify-callback-page/spotify-authentication-callback.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angulon-spotify-authentication-callback',
+  templateUrl: './spotify-authentication-callback.component.html',
+  styleUrls: ['./spotify-authentication-callback.component.scss'],
+})
+export class SpotifyAuthenticationCallbackComponent {}
