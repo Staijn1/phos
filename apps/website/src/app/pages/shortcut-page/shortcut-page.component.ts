c3e3508ce76e96c40436b63defccc338260b8645
Staijn1 <stein@jnkr.eu>
2023-06-27 20:41:34 +0200

    Hopefully fix issue where colors got set to null when using shortcuts

--- apps/website/src/app/pages/shortcut-page/shortcut-page.component.ts
+++ apps/website/src/app/pages/shortcut-page/shortcut-page.component.ts
@@ -1,47 +1,58 @@
-import {Component} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
-import {MessageService} from '../../services/message-service/message.service';
-import {WebsocketService} from "../../services/websocketconnection/websocket.service";
+import { Component } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { MessageService } from "../../services/message-service/message.service";
+import { WebsocketService } from "../../services/websocketconnection/websocket.service";
+import { Store } from "@ngrx/store";
+import { ColorpickerState } from "../../../redux/color/color.reducer";
 
 @Component({
-  selector: 'app-shortcut-page',
-  templateUrl: './shortcut-page.component.html',
-  styleUrls: ['./shortcut-page.component.scss'],
+  selector: "app-shortcut-page",
+  templateUrl: "./shortcut-page.component.html",
+  styleUrls: ["./shortcut-page.component.scss"]
 })
 export class ShortcutPageComponent {
   /**
    * From the query parameters we will read the shortcut to execute
    */
-  constructor(private activatedRoute: ActivatedRoute, private messageService: MessageService, private connection: WebsocketService, private router: Router) {
+  constructor(private activatedRoute: ActivatedRoute,
+              private messageService: MessageService,
+              private connection: WebsocketService,
+              private router: Router,
+              private store: Store<{ colorpicker: ColorpickerState }>) {
     this.activatedRoute.queryParams.subscribe(params => {
-      this.executeShortcut(params['action']);
+      this.executeShortcut(params["action"]);
     });
   }
 
   private executeShortcut(param: string | undefined) {
     if (!param) return;
 
+    let action: () => void;
     switch (param) {
       case "turnOff":
-        this.connection.turnOff();
+        action = this.connection.turnOff;
         break;
       case "increaseBrightness":
-        this.connection.increaseBrightness()
+        action = this.connection.increaseBrightness;
         break;
       case "decreaseBrightness":
-        this.connection.decreaseBrightness()
+        action = this.connection.decreaseBrightness;
         break;
       case "increaseSpeed":
-        this.connection.increaseSpeed();
+        action = this.connection.increaseSpeed;
         break;
       case "decreaseSpeed":
-        this.connection.decreaseSpeed();
+        action = this.connection.decreaseSpeed;
         break;
       default:
-        this.messageService.setMessage(new Error("Shortcut not found"))
+        action = () => this.messageService.setMessage(new Error("Shortcut not found"));
     }
-
-    // After the action just redirect to the home page
-    this.router.navigateByUrl('/home').catch(reason => this.messageService.setMessage(reason));
+    // We need to subscribe to the state of the color picker because the colors get set after the connection is established
+    this.store.select("colorpicker").subscribe((state) => {
+      console.log("Calling Action with state", state)
+      action();
+      // After the action just redirect to the home page
+      this.router.navigateByUrl("/home").catch(reason => this.messageService.setMessage(reason));
+    });
   }
 }
