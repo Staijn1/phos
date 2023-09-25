d5c1901e4127041cf913773e91a3a3cf5e0f61ba
Staijn1 <stein@jnkr.eu>
2023-09-26 00:05:42 +0200

    Implement color merge in redux

--- apps/website/src/redux/color/color.reducer.ts
+++ apps/website/src/redux/color/color.reducer.ts
@@ -5,17 +5,26 @@ export interface ColorpickerState {
 
 const initialState: ColorpickerState = {
   colors: ["#ff0000", "#00ff00", "#0000ff"],
-  updateLedstrips: true,
+  updateLedstrips: true
 };
 
 export function colorpickerReducer(state = initialState, action: any): ColorpickerState {
   switch (action.type) {
-    case 'COLOR_CHANGE':
+    case "COLOR_CHANGE": {
+      // merge the state.colors with the action.payload
+      const newColors = state.colors.map((color, index) => {
+        if (action.payload[index]) {
+          return action.payload[index];
+        } else {
+          return color;
+        }
+      });
       return {
         ...state,
-        colors: action.payload,
+        colors: newColors,
         updateLedstrips: action.updateLedstrips
       };
+    }
     default:
       return state;
   }
