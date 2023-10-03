31fdb2b14016d0d1bbba13f6b2dff1315c709f70
Stein Jonker <stein@jnkr.eu>
2023-10-03 09:55:22 +0200

    Add daisy ui and tailwind css

---
+++ apps/website/tailwind.config.js
@@ -0,0 +1,17 @@
+const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: "tw-",
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui")
+  ]
+};
