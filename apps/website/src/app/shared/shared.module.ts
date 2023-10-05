e122daea6697fbd7700d401ae6cd293a9840d48a
Staijn1 <stein@jnkr.eu>
2023-10-05 18:24:49 +0200

    Update shared.module.ts

--- apps/website/src/app/shared/shared.module.ts
+++ apps/website/src/app/shared/shared.module.ts
@@ -4,7 +4,6 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RouterModule } from '@angular/router';
 import { VisualizerComponent } from './components/visualizer/visualizer.component';
 import { CastPipe } from './pipe/CastPipe';
-import { ModalComponent } from './components/modal/modal.component';
 import { OffCanvasComponent } from './components/offcanvas/off-canvas.component';
 import { ColorpickerComponent } from './components/colorpicker/colorpicker.component';
 import { SpotifyPlayerComponent } from './components/spotify-player/spotify-player.component';
@@ -15,7 +14,6 @@ import { FormsModule } from '@angular/forms';
     ColorpickerComponent,
     VisualizerComponent,
     CastPipe,
-    ModalComponent,
     OffCanvasComponent,
     SpotifyPlayerComponent
   ],
@@ -24,7 +22,6 @@ import { FormsModule } from '@angular/forms';
     RouterModule,
     VisualizerComponent,
     CastPipe,
-    ModalComponent,
     OffCanvasComponent,
     ColorpickerComponent,
     SpotifyPlayerComponent
