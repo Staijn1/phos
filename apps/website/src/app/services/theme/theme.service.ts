0dbf2df92bf61100d170868579e35f712e47d05e
Staijn1 <stein@jnkr.eu>
2023-10-10 20:19:49 +0200

    Theme-color meta-tag was not working because CSS variables inside it is not supported

--- apps/website/src/app/services/theme/theme.service.ts
+++ apps/website/src/app/services/theme/theme.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { UserPreferences } from "../../shared/types/types";
-import { themes } from "../../shared/constants";
 import { MessageService } from "../message-service/message.service";
 
 @Injectable({
   providedIn: "root"
 })
 export class ThemeService {
-  private _theme = '';
+  private _theme = "";
   get theme(): string {
     return this._theme;
   }
@@ -27,6 +26,17 @@ export class ThemeService {
   applyTheme(theme: string) {
     // Set the data-theme attribute on the html tag. This will apply one of the (custom or built-in) daisyui themes
     document.documentElement.setAttribute("data-theme", theme);
+
+    // Get the theme-color meta-tag.
+    // The value of this tag will change the color of the address bar when installed as a PWA
+    const metaTag = document.querySelector("meta[name=\"theme-color\"]");
+
+    // Situation: We want to set the color of the address bar to the primary color of the theme. But the primary color is defined in CSS variables
+    // Problem: The meta-tag does not support CSS variables
+    // Solution: Get the value of the CSS variable and use it in the meta-tag to prevent usage of the var() function in the meta-tag
+    const primaryColor = getComputedStyle(document.documentElement).getPropertyValue("--p");
+    metaTag?.setAttribute("content", `hsl(${primaryColor}`);
+
     this._theme = theme;
   }
 }
