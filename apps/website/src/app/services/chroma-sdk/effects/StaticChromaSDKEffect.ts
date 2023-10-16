6afaa056c5079f5c90bdd7300781516f021d4441
Staijn1 <stein@jnkr.eu>
2023-10-16 19:33:23 +0200

    Implement mouse and headset too for static effect

--- apps/website/src/app/services/chroma-sdk/effects/StaticChromaSDKEffect.ts
+++ apps/website/src/app/services/chroma-sdk/effects/StaticChromaSDKEffect.ts
@@ -1,7 +1,7 @@
 import iro from "@jaames/iro";
 import { BaseChromaSDKEffect } from "./BaseChromaSDKEffect";
 import { calculateBGRInteger } from "../../../shared/functions";
-import { ChromaKeyboardEffectType } from "../../old/chromaSDK/old-chroma-s-d-k.service";
+import { ChromaKeyboardEffectType, HeadsetEffect, MouseEffect } from "../../old/chromaSDK/old-chroma-s-d-k.service";
 
 export class StaticChromaSDKEffect extends BaseChromaSDKEffect {
   onEntry(): void {
@@ -18,5 +18,7 @@ export class StaticChromaSDKEffect extends BaseChromaSDKEffect {
     const BGRColor = calculateBGRInteger(color.red, color.green, color.blue);
     this.connection
       .createKeyboardEffect(ChromaKeyboardEffectType.CHROMA_STATIC, BGRColor);
+    this.connection.createHeadsetEffect(HeadsetEffect.CHROMA_STATIC, BGRColor);
+    this.connection.createMouseEffect(MouseEffect.CHROMA_STATIC, BGRColor);
   }
 }
