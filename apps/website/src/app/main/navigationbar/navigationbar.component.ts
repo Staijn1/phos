5d0374a79df5a7720b0e6a1db399253eee817062
Staijn1 <stein@jnkr.eu>
2023-10-22 21:51:00 +0200

    Refactoring of mobile nav

--- apps/website/src/app/main/navigationbar/navigationbar.component.ts
+++ apps/website/src/app/main/navigationbar/navigationbar.component.ts
@@ -1,8 +1,5 @@
-import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
-import { gsap } from 'gsap';
-import { NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { Component, Input, ViewChild } from "@angular/core";
 import {
-  faBars as OpenMobileMenuIcon,
   faChartBar,
   faCog,
   faEyeDropper,
@@ -13,20 +10,18 @@ import {
   faPowerOff,
   faRunning,
   faSlidersH,
-  faTimes as CloseMobileMenuIcon,
   faWalking
-} from '@fortawesome/free-solid-svg-icons';
-import { Store } from '@ngrx/store';
-import { ColorpickerComponent } from '../../shared/components/colorpicker/colorpicker.component';
-import { LedstripState } from '@angulon/interfaces';
-import { WebsocketService } from '../../services/websocketconnection/websocket.service';
+} from "@fortawesome/free-solid-svg-icons";
+import { Store } from "@ngrx/store";
+import { ColorpickerComponent } from "../../shared/components/colorpicker/colorpicker.component";
+import { LedstripState } from "@angulon/interfaces";
+import { WebsocketService } from "../../services/websocketconnection/websocket.service";
 import {
   DecreaseLedstripBrightness,
   DecreaseLedstripSpeed,
   IncreaseLedstripBrightness,
   IncreaseLedstripSpeed
-} from '../../../redux/ledstrip/ledstrip.action';
-import { OffCanvasComponent } from '../../shared/components/offcanvas/off-canvas.component';
+} from "../../../redux/ledstrip/ledstrip.action";
 import {
   MAXIMUM_BRIGHTNESS,
   MINIMUM_BRIGHTNESS,
@@ -35,14 +30,14 @@ import {
 } from "../../shared/constants";
 
 @Component({
-  selector: 'app-navigationbar',
-  templateUrl: './navigationbar.component.html',
-  styleUrls: ['./navigationbar.component.scss']
+  selector: "app-navigationbar",
+  templateUrl: "./navigationbar.component.html",
+  styleUrls: ["./navigationbar.component.scss"]
 })
-export class NavigationbarComponent implements OnInit {
-  @ViewChild('mobileMenu') mobileMenu!: OffCanvasComponent;
-  @ViewChild('container') navContainer!: ElementRef;
+export class NavigationbarComponent {
   @ViewChild(ColorpickerComponent) colorpicker!: ColorpickerComponent;
+  @Input() colorPickerOrientation: "horizontal" | "vertical" = "horizontal";
+
   readonly homeIcon = faHome;
   readonly modeIcon = faList;
   readonly visualizerIcon = faChartBar;
@@ -54,56 +49,23 @@ export class NavigationbarComponent implements OnInit {
   readonly increaseBrightnessIcon = faPlus;
   readonly speedIncreaseIcon = faRunning;
   readonly speedDecreaseIcon = faWalking;
-  readonly timeline = gsap.timeline();
-  mobileMenuIcon = OpenMobileMenuIcon;
-  private animationMode = 0;
+
   minimumBrightnessReached = false;
   minimumSpeedReached = false;
   maximumBrightnessReached = false;
-  maximumSpeedReached=false;
+  maximumSpeedReached = false;
 
 
   constructor(
     public connection: WebsocketService,
-    private router: Router,
     private store: Store<{ ledstripState: LedstripState }>
   ) {
     store.select("ledstripState").subscribe(state => {
       this.minimumBrightnessReached = state.brightness === MINIMUM_BRIGHTNESS;
-      this.minimumSpeedReached  = state.speed === SPEED_MINIMUM_INTERVAL_MS;
+      this.minimumSpeedReached = state.speed === SPEED_MINIMUM_INTERVAL_MS;
       this.maximumBrightnessReached = state.brightness === MAXIMUM_BRIGHTNESS;
       this.maximumSpeedReached = state.speed === SPEED_MAXIMUM_INTERVAL_MS;
     });
-  }
-
-  ngOnInit(): void {
-    // Subscribe to router events so we can display a page transition animation each time the user navigates to a new page.
-    this.router.events.subscribe((val) => {
-      // When the user starts to navigate to a new page, immediately show the cover again otherwise content will already be visible.
-      if (val instanceof NavigationStart) {
-        gsap.set('#cover', { autoAlpha: 1, duration: 0.3 });
-      }
-      if (val instanceof NavigationEnd) {
-        this.animate();
-        this.mobileMenu.close();
-      }
-    });
-  }
-
-  /**
-   * On resize, check if the screen is still small enough for a mobile menu.
-   * If the screen is big and the menu was open, we will close it
-   * If the screen turned small and the menu is open, we will open it up
-   * @private
-   */
-  @HostListener('window:resize')
-  private closeMobileMenuOnBigScreen(): void {
-    if (screen.width > 992 && this.mobileMenu.isOpen) this.mobileMenu.close();
-    if (screen.width <= 992 && this.mobileMenu.isOpen) this.mobileMenu.open();
-  }
-
-  onMobileMenuChange(isOpen: boolean) {
-    this.mobileMenuIcon = isOpen ? CloseMobileMenuIcon : OpenMobileMenuIcon;
   }
 
   turnOff(): void {
@@ -124,61 +86,5 @@ export class NavigationbarComponent implements OnInit {
 
   decreaseSpeed() {
     this.store.dispatch(new DecreaseLedstripSpeed());
-  }
-
-  private animationFromLeft(): void {
-    // First we transform the tiles from the left to the right, staggered.
-    this.timeline.to('.from-left .tile', {
-      duration: 0.4,
-      width: '100%',
-      left: '0%',
-      delay: 0,
-      stagger: 0.05
-    });
-    // After that animation has finished, we transform the tiles from the right to the left, staggered.
-    this.timeline.to('.from-left .tile', {
-      duration: 0.4,
-      width: '100%',
-      left: '100%',
-      delay: 0,
-      stagger: -0.05
-    });
-    gsap.set('.from-left .tile', { left: '0', width: '0' });
-  }
-
-  private animate(): void {
-    // Animate the cover to fade out, revealing the page.
-
-    switch (this.animationMode) {
-      case 0:
-        this.animateFromTop();
-        break;
-      case 1:
-        this.animationFromLeft();
-        break;
-    }
-
-    this.timeline.to('#cover', { duration: 0.6, autoAlpha: 0, ease: 'power4.inOut' });
-    this.animationMode = ++this.animationMode % 2;
-  }
-
-  private animateFromTop(): void {
-    // Fist we transform the tiles from the top to the bottom, staggered.
-    this.timeline.to('.from-top .tile', {
-      duration: 0.4,
-      height: '100%',
-      top: '0%',
-      delay: 0,
-      stagger: 0.05
-    });
-    // After that animation has finished, we transform the tiles from the bottom to the top, staggered.
-    this.timeline.to('.from-top .tile', {
-      duration: 0.4,
-      height: '100%',
-      top: '100%',
-      delay: 0,
-      stagger: -0.05
-    });
-    gsap.set('.from-top .tile', { top: '0', height: '0' });
   }
 }
