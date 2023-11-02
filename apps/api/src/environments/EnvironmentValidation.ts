2252e53e658e2859bc96c4cf2e697ed9895c60e8
Stein Jonker <stein@jnkr.eu>
2023-11-02 19:58:53 +0100

    Fix startup issue for API

--- apps/api/src/environments/EnvironmentValidation.ts
+++ apps/api/src/environments/EnvironmentValidation.ts
@@ -1,13 +1,7 @@
-import {IDatabaseConfiguration, IEnvironmentConfiguration} from './IEnvironmentConfiguration';
-import {IsBoolean, IsNumber, IsObject, IsString} from 'class-validator';
-
-class EnvironmentConfiguration implements IEnvironmentConfiguration {
-  @IsBoolean()
-  production = false;
-
-  @IsObject()
-  database: DatabaseEnvironment;
-}
+import { constructEnvironmentConfiguration } from "./EnvironmentConfig";
+import { IDatabaseConfiguration, IEnvironmentConfiguration } from './IEnvironmentConfiguration';
+import { IsBoolean, IsNumber, IsObject, IsString, validateSync } from 'class-validator';
+import { plainToInstance } from 'class-transformer';
 
 class DatabaseEnvironment implements IDatabaseConfiguration {
   @IsString()
@@ -22,19 +16,29 @@ class DatabaseEnvironment implements IDatabaseConfiguration {
   password: string;
 }
 
+class EnvironmentConfiguration implements IEnvironmentConfiguration {
+  @IsBoolean()
+  production = false;
+
+  @IsObject()
+  database: DatabaseEnvironment;
+}
+
+
+
 /**
  * Validation function for the environment config which is possibly set by the environment.ts files or environment variables.
  * @throws Error if the validation fails.
  * @returns the validated environment configuration.
  */
 export function validateEnvironmentConfiguration() {
-  // const config = constructEnvironmentConfiguration();
-  // const validatedConfig = plainToInstance(EnvironmentConfiguration, config, {enableImplicitConversion: true});
-//   const errors = validateSync(validatedConfig, {skipMissingProperties: false});
-//
-//   if (errors.length > 0) {
-//     throw new Error(errors.toString());
-//   }
+  const config = constructEnvironmentConfiguration();
+  const validatedConfig = plainToInstance(EnvironmentConfiguration, config, {enableImplicitConversion: true});
+  const errors = validateSync(validatedConfig, {skipMissingProperties: false});
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
   return {};
 }
 
