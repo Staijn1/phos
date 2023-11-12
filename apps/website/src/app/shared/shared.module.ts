f520e1691ebb2ab16f7086d0b6e1f3a23432d086
Staijn1 <stein@jnkr.eu>
2023-11-12 19:31:14 +0100

    Put pipes in own folders

--- apps/website/src/app/shared/shared.module.ts
+++ apps/website/src/app/shared/shared.module.ts
@@ -3,13 +3,13 @@ import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RouterModule } from '@angular/router';
 import { VisualizerComponent } from './components/visualizer/visualizer.component';
-import { CastPipe } from './pipe/CastPipe';
+import { CastPipe } from './pipe/cast/CastPipe';
 import { OffCanvasComponent } from './components/offcanvas/off-canvas.component';
 import { ColorpickerComponent } from './components/colorpicker/colorpicker.component';
 import { SpotifyPlayerComponent } from './components/spotify-player/spotify-player.component';
 import { FormsModule } from '@angular/forms';
-import { MsToTimePipe } from './pipe/MsToTime';
-import { YesNoPipe } from './pipe/yes-no.pipe';
+import { MsToTimePipe } from './pipe/ms-to-time/MsToTime';
+import { YesNoPipe } from './pipe/yes-no/yes-no.pipe';
 import { MappedIPv6ToIPv4Pipe } from './pipe/mapped-ipv6-to-ipv4/mapped-ipv6-to-ipv4.pipe';
 
 @NgModule({
