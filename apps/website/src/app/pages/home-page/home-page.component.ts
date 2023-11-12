7ac2651feffceab9b4028068f228111b09bfb1ea
Staijn1 <stein@jnkr.eu>
2023-11-12 18:29:24 +0100

    Added an overview for connected devices

--- apps/website/src/app/pages/home-page/home-page.component.ts
+++ apps/website/src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,9 @@
-import { Component } from "@angular/core";
-import { LedstripState } from "@angulon/interfaces";
-import { Store } from "@ngrx/store";
-import { MAXIMUM_BRIGHTNESS, SPEED_MAXIMUM_INTERVAL_MS } from "../../shared/constants";
-import { DecimalPipe } from "@angular/common";
-import { RadialProgressComponent } from "../../shared/components/radialprogress/radial-progress.component";
+import {Component} from "@angular/core";
+import {INetworkState, LedstripState} from "@angulon/interfaces";
+import {Store} from "@ngrx/store";
+import {MAXIMUM_BRIGHTNESS, SPEED_MAXIMUM_INTERVAL_MS} from "../../shared/constants";
+import {DecimalPipe, NgClass, NgForOf, NgIf} from "@angular/common";
+import {RadialProgressComponent} from "../../shared/components/radialprogress/radial-progress.component";
 
 @Component({
   selector: "app-home",
@@ -11,12 +11,16 @@ import { RadialProgressComponent } from "../../shared/components/radialprogress/
   styleUrls: ["./home-page.component.scss"],
   imports: [
     DecimalPipe,
-    RadialProgressComponent
+    RadialProgressComponent,
+    NgForOf,
+    NgIf,
+    NgClass
   ],
   standalone: true
 })
 export class HomePageComponent {
   private ledstripState: LedstripState | undefined;
+  networkState: INetworkState | undefined;
 
   get speedPercentage() {
     return this.ledstripState ? this.ledstripState?.speed / SPEED_MAXIMUM_INTERVAL_MS * 100 : 0;
@@ -25,9 +29,10 @@ export class HomePageComponent {
   get brightnessPercentage() {
     return this.ledstripState ? this.ledstripState?.brightness / MAXIMUM_BRIGHTNESS * 100 : 0;
   }
-  constructor(private readonly store: Store<{ ledstripState: LedstripState }>) {
-    this.store.select("ledstripState").subscribe((state) => {
-      this.ledstripState = state;
-    });
+
+  constructor(private readonly store: Store<{ ledstripState: LedstripState, networkState: INetworkState }>) {
+    this.store.select("ledstripState").subscribe((state) => this.ledstripState = state);
+
+    this.store.select("networkState").subscribe((state) => this.networkState = state);
   }
 }
