dc607a74aeed5e21186583662fa483f6bb748394
Staijn1 <stein@jnkr.eu>
2023-11-16 21:58:52 +0100

    Removed old stuff

--- apps/website/src/app/pages/mode-page/mode-page.component.ts
+++ apps/website/src/app/pages/mode-page/mode-page.component.ts
@@ -1,23 +1,10 @@
-import { Component, OnDestroy, ViewChild } from "@angular/core";
-import { OldChromaEffectService } from "../../services/old/chromaEffect/old-chroma-effect.service";
-import { BlinkState } from "../../services/old/chromaEffect/state/blink-state/blink-state";
-import { SingleDynamicState } from "../../services/old/chromaEffect/state/single-dynamic-state/single-dynamic-state";
-import { MultiDynamicState } from "../../services/old/chromaEffect/state/multi-dynamic-state/multi-dynamic-state";
-import { RainbowState } from "../../services/old/chromaEffect/state/rainbow-state/rainbow-state";
-import { Fire2012State } from "../../services/old/chromaEffect/state/fire2012-state/fire2012-state";
-import { WaterfallState } from "../../services/old/chromaEffect/state/waterfall-state/waterfall-state";
-import { RainbowCycleState } from "../../services/old/chromaEffect/state/rainbow-cycle-state/rainbow-cycle-state";
-import { VisualizerState } from "../../services/old/chromaEffect/state/visualizer-state/visualizer-state";
-import { LedstripState, ModeInformation } from "@angulon/interfaces";
-import { ThemeService } from "../../services/theme/theme.service";
-import {
-  VisualizerBrightnessState
-} from "../../services/old/chromaEffect/state/visualizer-brightness-state/visualizer-brightness-state";
-import { ColorpickerComponent } from "../../shared/components/colorpicker/colorpicker.component";
-import { ChangeLedstripMode } from "../../../redux/ledstrip/ledstrip.action";
-import { Store } from "@ngrx/store";
-import { combineLatest } from "rxjs";
-import { NgClass, NgForOf, NgIf } from "@angular/common";
+import {Component, OnDestroy, ViewChild} from '@angular/core';
+import {LedstripState, ModeInformation} from '@angulon/interfaces';
+import {ColorpickerComponent} from '../../shared/components/colorpicker/colorpicker.component';
+import {ChangeLedstripMode} from '../../../redux/ledstrip/ledstrip.action';
+import {Store} from '@ngrx/store';
+import {combineLatest} from 'rxjs';
+import {NgClass, NgForOf, NgIf} from '@angular/common';
 
 
 @Component({
@@ -35,20 +22,6 @@ export class ModePageComponent implements OnDestroy {
   @ViewChild(ColorpickerComponent) colorpicker: ColorpickerComponent | undefined;
 
   modes: ModeInformation[] = [];
-  /**
-   * TODO: Re-implement using chroma-effects
-   */
-  chromaEffects = [
-    { name: 'Rainbow', state: new RainbowState() },
-    { name: 'Rainbow Cycle', state: new RainbowCycleState() },
-    { name: 'Multi Dynamic', state: new MultiDynamicState() },
-    { name: 'Single Dynamic', state: new SingleDynamicState() },
-    { name: 'Blink', state: new BlinkState() },
-    { name: 'Fire2012', state: new Fire2012State() },
-    { name: 'Waterfall', state: new WaterfallState() },
-    { name: 'VuMeter', state: new VisualizerState() },
-    { name: 'VuMeter Brightness', state: new VisualizerBrightnessState() }
-  ];
   iconBoxColors = [
     'primary',
     'secondary',
