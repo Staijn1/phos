dac1dea9635c071f598fde73f5649af032b21dd9
Staijn1 <stein@jnkr.eu>
2023-11-19 16:32:59 +0100

    Update room.module.ts

--- apps/api/src/app/room/room.module.ts
+++ apps/api/src/app/room/room.module.ts
@@ -2,9 +2,10 @@ import {Module} from '@nestjs/common';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import {Room} from './Room.model';
 import {RoomService} from './room.service';
+import {DeviceModule} from "../device/device.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Room])],
+  imports: [TypeOrmModule.forFeature([Room]), DeviceModule],
   providers: [RoomService],
   exports: [RoomService]
 })
