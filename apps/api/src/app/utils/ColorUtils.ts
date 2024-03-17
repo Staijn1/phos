9c144687ee0b03d3d2362efad472ad2e047b5bd6
Stein Jonker <stein@jnkr.eu>
2024-03-17 14:52:17 +0100

    Initial version of a method that can calculate an estimated power draw of each device

---
+++ apps/api/src/app/utils/ColorUtils.ts
@@ -0,0 +1,13 @@
+/**
+ * Convert Hex string to RGB array
+ * @param hex
+ * @returns [R, G, B] array where R,G,B values range from 0 to 255
+ */
+export const hexToRgb = (hex: string): number[] => {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? [
+    parseInt(result[1], 16),
+    parseInt(result[2], 16),
+    parseInt(result[3], 16)
+  ] : [0, 0, 0];
+}
