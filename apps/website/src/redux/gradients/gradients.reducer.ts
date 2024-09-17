10da8bb9f6975e825338518ea8db4605ca6c5ab1
Staijn1 <stein@jnkr.eu>
2024-09-17 17:38:10 +0200

    Fix list of gradients constantly growing for spotify gradients (#207)
    # Added
    
    # Changed
    
    # Fixed
    - Fix the list of gradients infinitely growing when playing spotify
    songs
    # Improved

--- apps/website/src/redux/gradients/gradients.reducer.ts
+++ apps/website/src/redux/gradients/gradients.reducer.ts
@@ -8,6 +8,11 @@ export const gradientsReducer = (state: GradientInformation[] = initialState, ac
     case GradientAction.LOAD_GRADIENTS:
       return action.payload;
     case GradientAction.REGISTER_GRADIENT:
+      // Add the new gradient to the list of gradients, if the gradient (by id) does not already exist
+      if (state.find((gradient) => gradient.id === action.payload.id)) {
+        return state;
+      }
+
       return [...state, action.payload];
     default:
       return state;
